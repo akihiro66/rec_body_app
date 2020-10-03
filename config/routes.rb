5f5c06b1c69790c3e57d8d140ac206c63d367bcc
Akihiro Matsumoto <wod.jep.kaw@gmail.com>
2020-10-03 14:50:28 +0900

    ゲストログイン機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+                       registrations: "users/registrations",
+                       passwords: "users/passwords",
+                     }
   root "graphs#index"
   resource :graphs, only: %i[index create update]
+
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#new_guest"
+  end
 end
